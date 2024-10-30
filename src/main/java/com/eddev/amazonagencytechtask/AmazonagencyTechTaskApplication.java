3ef1695b77aff79bf1a12400c377eccc6a936189
Eddie <partyka.eduard@gmail.com>
2024-10-30 20:47:42 +0200

    init

---
+++ src/main/java/com/eddev/amazonagencytechtask/AmazonagencyTechTaskApplication.java
@@ -0,0 +1,13 @@
+package com.eddev.amazonagencytechtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonagencyTechTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonagencyTechTaskApplication.class, args);
+    }
+
+}
